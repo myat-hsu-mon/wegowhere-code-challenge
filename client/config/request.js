c09318aaae98d6c11fe705ce1a2b073f1a111d61
myat-hsu-mon <47687494+myat-hsu-mon@users.noreply.github.com>
2023-12-16 11:22:37 +0630

    change ip address to localhost

--- client/config/request.js
+++ client/config/request.js
@@ -1,7 +1,7 @@
 import axios, { isAxiosError } from "axios";
 
 const request = axios.create({
-  baseURL: "http://192.168.174.34:3000/",
+  baseURL: "http://localhost:3000/",
   headers: { "Content-Type": "application/json" },
 });
 
