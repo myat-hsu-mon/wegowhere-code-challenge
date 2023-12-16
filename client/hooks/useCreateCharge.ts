c8f0eb5bcc5e0a9d87a15b4eb23f18e57ab2bee7
myat-hsu-mon <47687494+myat-hsu-mon@users.noreply.github.com>
2023-12-16 09:56:52 +0630

    feat: create use-create-charge hook

---
+++ client/hooks/useCreateCharge.ts
@@ -0,0 +1,43 @@
+import { useState, Dispatch, SetStateAction } from "react";
+
+import { PaymentCardProps } from "../interfaces/paymentCard";
+import request from "../config/request";
+
+interface createChargeProps {
+  card: PaymentCardProps;
+  amount: string;
+  currency: string;
+}
+
+interface UseCreatePaymentCardResult {
+  createCharge: (chargeInfo: createChargeProps) => Promise<void>;
+  loading: boolean;
+  error: string | null;
+  modalVisible: boolean;
+  setModalVisible: Dispatch<SetStateAction<boolean>>;
+}
+
+const useCreateCharge = (): UseCreatePaymentCardResult => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const createCharge = async (chargeInfo: createChargeProps) => {
+    try {
+      setLoading(true);
+      const response = await request.post(
+        "/charges",
+        JSON.stringify(chargeInfo)
+      );
+      setModalVisible(response.data.data?.paid);
+    } catch (error: any) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { createCharge, loading, error, modalVisible, setModalVisible };
+};
+
+export default useCreateCharge;
